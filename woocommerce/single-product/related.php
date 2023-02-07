4012082a4f12eb794a8e91fede63ddb3d78076fc
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2023-02-07 16:17:00 +0300

    ранее смотрели

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Related Products
  *
@@ -15,38 +16,41 @@
  * @version     3.9.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-$related_product_view = woodmart_get_opt( 'related_product_view' );
+$related_product_view = woodmart_get_opt('related_product_view');
 
-function woocommerceCategorySlug( $id ){
-    $term = get_term_by('id', $id, 'product_cat', 'ARRAY_A');
-    return $term['slug'];       
+function woocommerceCategorySlug($id)
+{
+	$term = get_term_by('id', $id, 'product_cat', 'ARRAY_A');
+	return $term['slug'];
 }
 
-function get_related_ids( $link ) {
+function get_related_ids($link)
+{
 
 	$ids = array();
 
-    $dom = new domDocument;
-    @$dom->loadHTML(file_get_contents($link));
-    $dom->preserveWhiteSpace = false;
-    $finder = new DomXPath($dom);
+	$dom = new domDocument;
+	@$dom->loadHTML(file_get_contents($link));
+	$dom->preserveWhiteSpace = false;
+	$finder = new DomXPath($dom);
 
 	$classname = "product-grid-item";
 	$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
-      
-    foreach ($nodes as $item) {
-        $ids[] = $item->getAttribute('data-id');
-    }
-      
-    return $ids;
+
+	foreach ($nodes as $item) {
+		$ids[] = $item->getAttribute('data-id');
+	}
+
+	return $ids;
 }
 
-function isMobile() {
-    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+function isMobile()
+{
+	return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
 
 global $product;
@@ -68,15 +72,14 @@ shuffle($ids);
 $related_products = array();
 $similar_products = array();
 
-$_pf = new WC_Product_Factory(); 
+$_pf = new WC_Product_Factory();
 
 foreach ($ids as $id) {
 
-    if (count($related_products) < 12) {
-    	if ($id != $product->get_id())
-    	$related_products[] = $_pf->get_product($id);
-    }
-
+	if (count($related_products) < 12) {
+		if ($id != $product->get_id())
+			$related_products[] = $_pf->get_product($id);
+	}
 }
 
 $sim_ids = array(
@@ -85,7 +88,8 @@ $sim_ids = array(
 		'530' => array(43604, 43614, 43609, 43599),
 		'182' => array(41710, 41706, 41095, 40789),
 		'240' => array(41448, 40247, 41553, 40244),
-		'462' => array(41672, 41662, 41643, 40588)),
+		'462' => array(41672, 41662, 41643, 40588)
+	),
 	'woman' => array(
 		'378' => array(40154, 40117, 40110, 40096),
 		'530' => array(41571, 41571, 41571, 41571),
@@ -96,14 +100,13 @@ $sim_ids = array(
 
 foreach ($sim_ids[$product_sex] as $sim_cat => $sim_id) {
 
-	$similar_id = $sim_id[rand(0,3)];
+	$similar_id = $sim_id[rand(0, 3)];
 
-    if (count($similar_products) < 5) {
-    	if ($similar_id != $product->get_id() AND $sim_cat != $product->get_category_ids()[0]) {
-    		$similar_products[] = $_pf->get_product($similar_id);
-    	}
-    }
-
+	if (count($similar_products) < 5) {
+		if ($similar_id != $product->get_id() and $sim_cat != $product->get_category_ids()[0]) {
+			$similar_products[] = $_pf->get_product($similar_id);
+		}
+	}
 }
 
 ?>
@@ -111,100 +114,107 @@ foreach ($sim_ids[$product_sex] as $sim_cat => $sim_id) {
 <div id="similar-products" class="related-products">
 	<h3 class="title slider-title">С этим товаром покупали</h3>
 	<?php
-		woodmart_enqueue_inline_style( 'product-loop' );
-		if ( !isMobile() ) {
-			$slider_args = array(
-				'slides_per_view' => ( woodmart_get_opt( 'related_product_columns' ) ) ? woodmart_get_opt( 'related_product_columns' ) : apply_filters( 'woodmart_related_products_per_view', 4 ),
-				'img_size' => 'woocommerce_thumbnail',
-				'products_bordered_grid' => woodmart_get_opt( 'products_bordered_grid' ),
-				'custom_sizes' => apply_filters( 'woodmart_product_related_custom_sizes', false ),
-				'product_quantity' => woodmart_get_opt( 'product_quantity' ),
-			);
-			
-			woodmart_set_loop_prop( 'products_view', 'carousel' );
+	woodmart_enqueue_inline_style('product-loop');
+	if (!isMobile()) {
+		$slider_args = array(
+			'slides_per_view' => (woodmart_get_opt('related_product_columns')) ? woodmart_get_opt('related_product_columns') : apply_filters('woodmart_related_products_per_view', 4),
+			'img_size' => 'woocommerce_thumbnail',
+			'products_bordered_grid' => woodmart_get_opt('products_bordered_grid'),
+			'custom_sizes' => apply_filters('woodmart_product_related_custom_sizes', false),
+			'product_quantity' => woodmart_get_opt('product_quantity'),
+		);
 
-			echo woodmart_generate_posts_slider( $slider_args, false, $similar_products );
-		} 
-		else {
-			woodmart_set_loop_prop( 'products_columns', woodmart_get_opt( 'related_product_columns' ) );
-			woodmart_set_loop_prop( 'products_different_sizes', false );
-			woodmart_set_loop_prop( 'products_masonry', false );
-			woodmart_set_loop_prop( 'products_view', 'grid' );
-			
-			woocommerce_product_loop_start();
+		woodmart_set_loop_prop('products_view', 'carousel');
 
-			foreach ( $similar_products as $similar_product ) {
-				$post_object = get_post( $similar_product->get_id() );
+		echo woodmart_generate_posts_slider($slider_args, false, $similar_products);
+	} else {
+		woodmart_set_loop_prop('products_columns', woodmart_get_opt('related_product_columns'));
+		woodmart_set_loop_prop('products_different_sizes', false);
+		woodmart_set_loop_prop('products_masonry', false);
+		woodmart_set_loop_prop('products_view', 'grid');
 
-				setup_postdata( $GLOBALS['post'] = $post_object );
+		woocommerce_product_loop_start();
 
-				wc_get_template_part( 'content', 'product' ); 
-			}
+		foreach ($similar_products as $similar_product) {
+			$post_object = get_post($similar_product->get_id());
 
-			woocommerce_product_loop_end();
-			
-			woodmart_reset_loop();
-			
-			if ( function_exists( 'woocommerce_reset_loop' ) ) woocommerce_reset_loop();
+			setup_postdata($GLOBALS['post'] = $post_object);
+
+			wc_get_template_part('content', 'product');
 		}
-		
+
+		woocommerce_product_loop_end();
+
+		woodmart_reset_loop();
+
+		if (function_exists('woocommerce_reset_loop')) woocommerce_reset_loop();
+	}
+
 	?>
 </div>
 
 
-<?php if ( $related_products ) : ?>
+<?php if ($related_products) : ?>
 
 	<div class="related-products">
 
 		<?php
-		$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related products', 'woocommerce' ) );
-		
-		if ( $heading ) :
-			?>
-		<h3 class="title slider-title"><?php echo esc_html( $heading ); ?></h3>
+		$heading = apply_filters('woocommerce_product_related_products_heading', __('Related products', 'woocommerce'));
+
+		if ($heading) :
+		?>
+			<h3 class="title slider-title"><?php echo esc_html($heading); ?></h3>
 		<?php endif; ?>
-		
+
 		<?php
-			woodmart_enqueue_inline_style( 'product-loop' );
-			if ( $related_product_view == 'slider' ) {
-				$slider_args = array(
-					'slides_per_view' => ( woodmart_get_opt( 'related_product_columns' ) ) ? woodmart_get_opt( 'related_product_columns' ) : apply_filters( 'woodmart_related_products_per_view', 4 ),
-					'img_size' => 'woocommerce_thumbnail',
-					'products_bordered_grid' => woodmart_get_opt( 'products_bordered_grid' ),
-					'custom_sizes' => apply_filters( 'woodmart_product_related_custom_sizes', false ),
-					'product_quantity' => woodmart_get_opt( 'product_quantity' ),
-				);
-				
-				woodmart_set_loop_prop( 'products_view', 'carousel' );
+		woodmart_enqueue_inline_style('product-loop');
+		if ($related_product_view == 'slider') {
+			$slider_args = array(
+				'slides_per_view' => (woodmart_get_opt('related_product_columns')) ? woodmart_get_opt('related_product_columns') : apply_filters('woodmart_related_products_per_view', 4),
+				'img_size' => 'woocommerce_thumbnail',
+				'products_bordered_grid' => woodmart_get_opt('products_bordered_grid'),
+				'custom_sizes' => apply_filters('woodmart_product_related_custom_sizes', false),
+				'product_quantity' => woodmart_get_opt('product_quantity'),
+			);
 
-				echo woodmart_generate_posts_slider( $slider_args, false, $related_products );
-			} elseif ( $related_product_view == 'grid' ) {
-		
-				woodmart_set_loop_prop( 'products_columns', woodmart_get_opt( 'related_product_columns' ) );
-				woodmart_set_loop_prop( 'products_different_sizes', false );
-				woodmart_set_loop_prop( 'products_masonry', false );
-				woodmart_set_loop_prop( 'products_view', 'grid' );
-				
-				woocommerce_product_loop_start();
+			woodmart_set_loop_prop('products_view', 'carousel');
 
-				foreach ( $related_products as $related_product ) {
-					$post_object = get_post( $related_product->get_id() );
+			echo woodmart_generate_posts_slider($slider_args, false, $related_products);
+		} elseif ($related_product_view == 'grid') {
 
-					setup_postdata( $GLOBALS['post'] = $post_object );
+			woodmart_set_loop_prop('products_columns', woodmart_get_opt('related_product_columns'));
+			woodmart_set_loop_prop('products_different_sizes', false);
+			woodmart_set_loop_prop('products_masonry', false);
+			woodmart_set_loop_prop('products_view', 'grid');
 
-					wc_get_template_part( 'content', 'product' ); 
-				}
+			woocommerce_product_loop_start();
 
-				woocommerce_product_loop_end();
-				
-				woodmart_reset_loop();
-				
-				if ( function_exists( 'woocommerce_reset_loop' ) ) woocommerce_reset_loop();
+			foreach ($related_products as $related_product) {
+				$post_object = get_post($related_product->get_id());
+
+				setup_postdata($GLOBALS['post'] = $post_object);
+
+				wc_get_template_part('content', 'product');
 			}
-			
+
+			woocommerce_product_loop_end();
+
+			woodmart_reset_loop();
+
+			if (function_exists('woocommerce_reset_loop')) woocommerce_reset_loop();
+		}
+
 		?>
 	</div>
 
 <?php endif;
+?>
 
+<div class="ranee">
+
+	<?php echo do_shortcode('[recently_viewed_products]'); ?>
+
+</div>
+
+<?php
 wp_reset_postdata();
